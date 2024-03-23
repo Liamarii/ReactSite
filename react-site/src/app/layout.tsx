b47bc4313e9acbeddfb2b62f41fd231249387a7a
Liamarii <39436443+Liamarii@users.noreply.github.com>
2024-03-23 23:12:01 +0000

    Add dark mode button

--- react-site/src/app/layout.tsx
+++ react-site/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { MainNavMenu } from "@/custom-components/navigation-bar/navigation-bar";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const fontSans = FontSans({
 	subsets: ["latin"],
@@ -20,14 +21,20 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning>
 			<body
 				className={cn(
 					"min-h-screen bg-background font-sans antialiased",
 					fontSans.variable
 				)}>
-				<MainNavMenu />
-				{children}
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange>
+					<MainNavMenu />
+					{children}
+				</ThemeProvider>
 			</body>
 		</html>
 	);
--- react-site/src/custom-components/navigation-bar/navigation-bar.tsx
+++ react-site/src/custom-components/navigation-bar/navigation-bar.tsx
@@ -13,6 +13,7 @@ import {
 	NavigationMenuTrigger,
 	navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
+import { ThemeToggle } from "../buttons/colour-scheme-button";
 
 const components: { title: string; href: string; description: string }[] = [
 	{
@@ -22,72 +23,75 @@ const components: { title: string; href: string; description: string }[] = [
 	},
 	{
 		title: "Page Two",
-		href: "/pages/pageTwo",
+		href: "/pageTwo",
 		description: "This links to the second page",
 	},
 	{
 		title: "Page Three",
-		href: "/pages/pageThree",
+		href: "/pageThree",
 		description: "This links to the third page",
 	},
 ];
 
 export function MainNavMenu() {
 	return (
-		<NavigationMenu>
-			<NavigationMenuList>
-				<NavigationMenuItem>
-					<NavigationMenuTrigger>Pages</NavigationMenuTrigger>
-					<NavigationMenuContent>
-						<ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-							<li className="row-span-3">
-								<NavigationMenuLink asChild>
-									<a
-										className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-										href="/">
-										<div className="mb-2 mt-4 text-lg font-medium">Logo</div>
-										<p className="text-sm leading-tight text-muted-foreground">
-											This is where the logo goes
-										</p>
-									</a>
-								</NavigationMenuLink>
-							</li>
-							<ListItem href="/" title="Page One">
-								This links to the first page
-							</ListItem>
-							<ListItem href="/pages/pageTwo" title="Page Two">
-								This links to the second page
-							</ListItem>
-							<ListItem href="/pages/pageThree" title="Page Three">
-								This links to the thrid page
-							</ListItem>
-						</ul>
-					</NavigationMenuContent>
-				</NavigationMenuItem>
-				<NavigationMenuItem>
-					<NavigationMenuTrigger>Pages</NavigationMenuTrigger>
-					<NavigationMenuContent>
-						<ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-							{components.map((component) => (
-								<ListItem
-									key={component.title}
-									title={component.title}
-									href={component.href}>
-									{component.description}
+		<>
+			<NavigationMenu>
+				<ThemeToggle />
+				<NavigationMenuList>
+					<NavigationMenuItem>
+						<NavigationMenuTrigger>Pages</NavigationMenuTrigger>
+						<NavigationMenuContent>
+							<ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+								<li className="row-span-3">
+									<NavigationMenuLink asChild>
+										<a
+											className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+											href="/">
+											<div className="mb-2 mt-4 text-lg font-medium">Logo</div>
+											<p className="text-sm leading-tight text-muted-foreground">
+												This is where the logo goes
+											</p>
+										</a>
+									</NavigationMenuLink>
+								</li>
+								<ListItem href="/" title="Page One">
+									This links to the first page
 								</ListItem>
-							))}
-						</ul>
-					</NavigationMenuContent>
-				</NavigationMenuItem>
-				<NavigationMenuItem>
-					<Link href="pages/pageThree" legacyBehavior passHref>
-						<NavigationMenuLink className={navigationMenuTriggerStyle()}>
-							PageThree
-						</NavigationMenuLink>
-					</Link>
-				</NavigationMenuItem>
-			</NavigationMenuList>
-		</NavigationMenu>
+								<ListItem href="/pageTwo" title="Page Two">
+									This links to the second page
+								</ListItem>
+								<ListItem href="/pageThree" title="Page Three">
+									This links to the thrid page
+								</ListItem>
+							</ul>
+						</NavigationMenuContent>
+					</NavigationMenuItem>
+					<NavigationMenuItem>
+						<NavigationMenuTrigger>Pages</NavigationMenuTrigger>
+						<NavigationMenuContent>
+							<ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+								{components.map((component) => (
+									<ListItem
+										key={component.title}
+										title={component.title}
+										href={component.href}>
+										{component.description}
+									</ListItem>
+								))}
+							</ul>
+						</NavigationMenuContent>
+					</NavigationMenuItem>
+					<NavigationMenuItem>
+						<Link href="/pageThree" legacyBehavior passHref>
+							<NavigationMenuLink className={navigationMenuTriggerStyle()}>
+								PageThree
+							</NavigationMenuLink>
+						</Link>
+					</NavigationMenuItem>
+				</NavigationMenuList>
+			</NavigationMenu>
+		</>
 	);
 }
 
